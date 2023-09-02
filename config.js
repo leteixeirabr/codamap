7887b96cfdcd3d409aa3362d688cbf8fc910d661
leteixeirabr <143885166+leteixeirabr@users.noreply.github.com>
2023-09-02 21:04:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-var googleMapsApiKey = AIzaSyBIycIFGctggBUZmtOLUIYL3fOJucFLRN0";
+var googleMapsApiKey = AIzaSyBIycIFGctggBUZmtOLUIYL3fOJucFLRN0;
 var codaApiToken = "e34e4706-e5cd-47a2-b8ce-2c4ac566e634";
 var codaApiUrl = "https://coda.io/apis/v1/docs/Ur_U805_WO/tables/table-TU86XjKyBJ/rows?useColumnNames=true";
